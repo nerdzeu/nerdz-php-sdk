4bda391b189071bf67ca682e62fc04a19d8e09b3
Riccardo Bonafede <bonaff@live.it>
2016-02-23 13:22:35 +0100

    added NerdzVideo class

---
+++ src/NERDZ/FileUpload/FacebookVideo.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace NERDZ\FileUpload;
+
+/**
+ * Class NERDZVideo
+ *
+ * @package NERDZ
+ */
+class NERDZVideo extends NERDZFile{
+}
--- src/NERDZ/NERDZRequest.php
+++ src/NERDZ/NERDZRequest.php
@@ -1,26 +1,5 @@
 <?php
-/**
- * Copyright 2014 NERDZ, Inc.
- *
- * You are hereby granted a non-exclusive, worldwide, royalty-free license to
- * use, copy, modify, and distribute this software in source code or binary
- * form for use in connection with the web services and APIs provided by
- * NERDZ.
- *
- * As with any software that integrates with the NERDZ platform, your use
- * of this software is subject to the NERDZ Developer Principles and
- * Policies [http://developers.NERDZ.com/policy/]. This copyright notice
- * shall be included in all copies or substantial portions of the software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- */
+
 namespace NERDZ;
 
 use NERDZ\Authentication\AccessToken;
@@ -477,7 +456,6 @@ class NERDZRequest
     {
         $this->validateMethod();
 
-        $graphVersion = NERDZUrlManipulator::forceSlashPrefix($this->graphVersion);
         $endpoint = NERDZUrlManipulator::forceSlashPrefix($this->getEndpoint());
 
         $url = $graphVersion . $endpoint;
@@ -498,7 +476,7 @@ class NERDZRequest
     public static function getDefaultHeaders()
     {
         return [
-            'User-Agent' => 'fb-php-' . NERDZ::VERSION,
+            'User-Agent' => 'nz-php-' . NERDZ::VERSION,
             'Accept-Encoding' => '*',
         ];
     }
